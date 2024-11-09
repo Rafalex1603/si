5c5f682811aeb9dcedd79e67aa0c34be37c8de7e
unknown <rafalexwtf@gmail.com>
2024-11-09 17:22:47 -0400

    hola

---
+++ twitter/post/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=40)
+    descripcion = models.TextField()
+    imagen=models.ImageField(upload_to="photos", blank=True)
+    precio=models.FloatField(default=True)
+    author = models.ForeignKey("auth.User",
+        on_delete= models.CASCADE,default=True)
+
+
+    class Meta: 
+        verbose_name_plural = "Publicaciones"
+
+    def __str__(self) -> str:
+        return self.titulo 
+    
+class Personas(models.Model):
+
+    imagen=models.ImageField(upload_to="photos", blank=True)
+    nombre=models.CharField(max_length=50)
+    descripcion=models.CharField(max_length=200)
+    class Meta:
+        verbose_name_plural= "perosnal"
+        
+    
+    def __str__(self):
+        
+        return self.nombre
